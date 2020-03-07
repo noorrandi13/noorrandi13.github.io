50bca7ed7691c719f44f55f763affbfa550475e2
Muhammad Randi Noor <noorrandi13@gmail.com>
2020-03-07 12:23:00 +0700

    tes

---
+++ frontend/nuxtjs/pages/helloworld.vue
@@ -0,0 +1,27 @@
+<template>
+  <article>
+    <section>
+      <h1>Get API Result</h1>
+      <div>
+        <p>{{ title }}</p>
+      </div>
+    </section>
+  </article>
+</template>
+
+<script>
+// import axios from '@nuxtjs/axios'
+import axios from 'axios'
+
+export default {
+  async asyncData() {
+    const { data } = await axios.get(
+      'http://localhost/coba-yii-integration/web/index.php?r=site/hello-world'
+    )
+
+    return { title: data.text }
+  }
+}
+</script>
+
+<style></style>
